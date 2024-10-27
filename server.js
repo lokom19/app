ebd17fc11926548b48f4da6fe442e8d3d05c61cf
lokom19 <87856931+lokom19@users.noreply.github.com>
2024-10-27 17:21:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,13 @@ function saveUserData(users) {
 
 let users = loadUserData();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use(express.static('.'));
 
 io.on('connection', (socket) => {
